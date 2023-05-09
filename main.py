cc29cdfff1a73e3efb3157cf6d6fbe9a71c88a0e
zhushanbo <zhushanbo@baidu.com>
2023-05-09 13:51:43 +0800

    no message

--- main.py
+++ main.py
@@ -65,9 +65,9 @@ class Matrix(object):
             if row.name == "对比":
                 continue
             print("")
-            print(row.name, "           Android / iOS")
-            print("搜索结果页           ", row.key0)
-            print("搜索H5落地页         ", row.key1)
+            print(row.name, "Android / iOS")
+            print("搜索结果页", row.key0)
+            print("搜索H5落地页", row.key1)
             print("搜索NA落地页（百家号）", row.key2)
 
 if __name__ == '__main__':
