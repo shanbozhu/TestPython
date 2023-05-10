efd4b309982adafb6bce4a812e2c4db9014ae45c
zhushanbo <zhushanbo@baidu.com>
2023-05-10 10:14:59 +0800

    no message

--- msearch_performance.py
+++ msearch_performance.py
@@ -9,10 +9,17 @@ Description:
 极速版 - 搜索结果页\h5落地页\百家号落地页上屏耗时
 """
 
+
+#  $ ./msearch_performance.py
+# /usr/local/bin/python: can't open file '/Users/wsc/myshells/main_lite_search.py': [Errno 2] No such file or directory
+# /usr/local/bin/python: can't open file '/Users/wsc/myshells/main_tomas_search_result_page.py': [Errno 2] No such file or directory
+# /usr/local/bin/python: can't open file '/Users/wsc/myshells/main_tomas_search_imagetext_landing_page.py': [Errno 2] No such file or directory
+# /usr/local/bin/python: can't open file '/Users/wsc/myshells/main_tomas_search_h5_landing_page.py': [Errno 2] No such file or directory
+
 import os
 
 # /Users/wsc/Desktop/Test/TestPython/main_lite_search.py
-os.system("python main_lite_search.py")
-os.system("python main_tomas_search_result_page.py")
-os.system("python main_tomas_search_imagetext_landing_page.py")
-os.system("python main_tomas_search_h5_landing_page.py")
+os.system("python /Users/wsc/Desktop/Test/TestPython/main_lite_search.py")
+os.system("python /Users/wsc/Desktop/Test/TestPython/main_tomas_search_result_page.py")
+os.system("python /Users/wsc/Desktop/Test/TestPython/main_tomas_search_imagetext_landing_page.py")
+os.system("python /Users/wsc/Desktop/Test/TestPython/main_tomas_search_h5_landing_page.py")
