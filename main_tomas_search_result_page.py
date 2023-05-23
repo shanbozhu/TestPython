f92e9eb7ad32c1a12d9c5f134dd251404c6c39ef
zhushanbo <zhushanbo@baidu.com>
2023-05-23 21:10:00 +0800

    no message

--- main_tomas_search_result_page.py
+++ main_tomas_search_result_page.py
@@ -93,7 +93,7 @@ class Matrix(object):
         print("\"大字版\"查询日期:", date)
 
         # 请求路径,请求参数
-        url = "https://sugar.baidu-int.com/api/report/r_1013e-1ntg07be-kr4bje/chart-data/c_1013e-2s170c5r-k49mmb"
+        url = "https://sugar.baidu-int.com/api/reportShare/325ba1e2445bc3cc51ac10d906d13dde/report/r_1013e-1ntg07be-kr4bje/chart-data/c_1013e-2s170c5r-k49mmb"
 
         # print("请输入\"大字版\"查询日期后回车(输入格式如: 2023-04-28,2023-05-04):")
         # date = input()
@@ -104,23 +104,13 @@ class Matrix(object):
         # 请求头
         header = {
             "Content-Type": "application/json",
-            "Cookie": "UUAP_TRACE_TOKEN=033821185ba45b8e95a4bc89711c8cd3; jsdk-uuid=5ca8089c-e694-43b9-9e7c-94f57a5e069c; sugar-company=Baidu; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22186c5bf5f33112-024e0771d9173f4-1f525634-2007040-186c5bf5f3534f%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%7D%2C%22%24device_id%22%3A%22186c5bf5f33112-024e0771d9173f4-1f525634-2007040-186c5bf5f3534f%22%7D; Hm_lvt_822e11c38954ecab94ff3f058fe8c079=1677655634,1679540732; jsdk-user=muBLyQjMmnC1/NTAncVKqA==; _csrf=n-amXNHnbyEb0vvBIWsEeRPG; SECURE_UUAP_P_TOKEN=PT-854704388101201922-2EJ2wOKQFq-uuap; UUAP_P_TOKEN=PT-854704388101201922-2EJ2wOKQFq-uuap; bdWikiBusinessUserGuid=16QIi08Fi0; sugarbisid=s%3ALiZIkCP7h8tkPlYX0NWeRHemwCC1Qdt3.uRJ17c2bNLy2Ota56HmRZF%2B67Tyb9d39rVG5rATMowM; RT=\"z=1&dm=baidu-int.com&si=744d22b3-88f4-4be3-99e7-c06798942e10&ss=lgxmsrce&sl=s&tt=fnz&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&nu=pp706064&cl=1jx0w&ld=1jxnl\"; UUAP_S_TOKEN=ST-858081831659601921-x2Acc-uuap",
+            "Cookie": "SECURE_UUAP_P_TOKEN=PT-867494923821858816-cC5Wv6wHxh-uuap; SECURE_BSG_B_TOKEN=zEWgXpxO4xvG/2juMkPTf+B8C63lX09TVjyUebNzbIJ57tks6GWyFhX6E8AHb5Ye0yrkH8jsplnBAYYlyvTVzA==",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
         }
 
         # 请求体
         version = self.request_version()
-        body = {"conditions": [{"k": "dateRange", "t": "dateRange", "v": date},
-                               {"k": "event_day", "t": "date", "v": end_date},
-                               {"k": "compare_event_day", "t": "date", "v": end_date},
-                               {"k": "app_id", "t": "select", "v": "12117"},
-                               {"k": "search_page", "t": "select", "v": "all"},
-                               {"k": "search_source", "t": "select", "v": "all"},
-                               {"k": "soft_version", "t": "select", "v": version},
-                               {"k": "net_type", "t": "select", "v": "all"},
-                               {"k": "device_level", "t": "select", "v": "all"}],
-                "conditionsDisplayValue": {"app_id": "手百大字版"}, "resourceHash": "c_1013e-2s170c5r-k49mmb",
-                "pageHash": "r_1013e-1ntg07be-kr4bje"}
+        body = {"conditions":[{"k":"dateRange","t":"dateRange","v":date},{"k":"event_day","t":"date","v":end_date},{"k":"compare_event_day","t":"date","v":end_date},{"k":"app_id","t":"select","v":"12117"},{"k":"search_page","t":"select","v":"all"},{"k":"search_source","t":"select","v":"all"},{"k":"soft_version","t":"select","v":version},{"k":"net_type","t":"select","v":"all"},{"k":"device_level","t":"select","v":"all"}],"conditionsDisplayValue":{"app_id":"手百大字版"},"o":"performance.baidu.com","resourceHash":"c_1013e-2s170c5r-k49mmb","pageHash":"r_1013e-1ntg07be-kr4bje"}
 
         # 请求方式
         resp = requests.post(url, json=body, headers=header, timeout=10, verify=False)
