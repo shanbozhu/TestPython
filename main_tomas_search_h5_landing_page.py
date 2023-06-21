4f56c85f8205191aff6436f20d13759b2735d5ac
zhushanbo <zhushanbo@baidu.com>
2023-06-21 17:05:28 +0800

    no message

--- main_tomas_search_h5_landing_page.py
+++ main_tomas_search_h5_landing_page.py
@@ -47,7 +47,7 @@ class Matrix(object):
     def __init__(self):
         pass
 
-    def request_data(self):
+    def request_data(self, product, v_appid, app_id):
         today = dt.date.today()
         yesterday = today - dt.timedelta(days=1)
         # 相对于昨天的6天前日期
@@ -55,7 +55,7 @@ class Matrix(object):
         end_date = str(yesterday)
         start_date = str(week_ago)
         date = start_date + "," + end_date
-        print("\"大字版\"查询日期:", date)
+        print(app_id, "查询日期:", date)
 
         # 请求路径,请求参数
         url = "https://sugar.baidu-int.com/api/reportShare/3a6c2f8b21f8a45927ecb1d95a1af23e/report/r_1013e-8ad82r3p-o8ll5x/chart-data/c_1013e-c254mx61-kepbl7"
@@ -74,8 +74,8 @@ class Matrix(object):
         }
 
         # 请求体
-        version = main_tomas_search_result_page.Matrix().request_version()
-        body = {"conditions":[{"k":"dateRange","t":"dateRange","v":date},{"k":"event_day","t":"date","v":end_date},{"k":"compare_event_day","t":"date","v":end_date},{"k":"app_id","t":"select","v":"12117"},{"k":"search_page","t":"select","v":"all"},{"k":"soft_version","t":"select","v":version},{"k":"net_type","t":"select","v":"all"},{"k":"device_level","t":"select","v":"all"},{"k":"pd","t":"select","v":"all"},{"k":"atn","t":"select","v":"all"},{"k":"status","t":"select","v":"all"}],"conditionsDisplayValue":{"app_id":"手百大字版"},"o":"performance.baidu.com","resourceHash":"c_1013e-c254mx61-kepbl7","pageHash":"r_1013e-8ad82r3p-o8ll5x"}
+        version = main_tomas_search_result_page.Matrix().request_version(product=product)
+        body = {"conditions":[{"k":"dateRange","t":"dateRange","v":date},{"k":"event_day","t":"date","v":end_date},{"k":"compare_event_day","t":"date","v":end_date},{"k":"app_id","t":"select","v":v_appid},{"k":"search_page","t":"select","v":"all"},{"k":"soft_version","t":"select","v":version},{"k":"net_type","t":"select","v":"all"},{"k":"device_level","t":"select","v":"all"},{"k":"pd","t":"select","v":"all"},{"k":"atn","t":"select","v":"all"},{"k":"status","t":"select","v":"all"}],"conditionsDisplayValue":{"app_id":app_id},"o":"performance.baidu.com","resourceHash":"c_1013e-c254mx61-kepbl7","pageHash":"r_1013e-8ad82r3p-o8ll5x"}
 
         # 请求方式
         resp = requests.post(url, json=body, headers=header, timeout=10, verify=False)
@@ -98,9 +98,12 @@ class Matrix(object):
             num += row.quantile_80
         num = num / len(data_row)
         # print("")
-        print("Tomas - H5落地页速度7日均值", str(round(num)) + "ms", ", 最大pv版本", version)
+        print("H5落地页速度7日均值", str(round(num)) + "ms", ", 最大pv版本", version)
         print("")
 
 if __name__ == '__main__':
     ma = Matrix()
-    ma.request_data()
+    # ma.request_data()
+    ma.request_data(product="tomas", v_appid="12117", app_id="手百大字版")
+    ma.request_data(product="baiduboxlite", v_appid="10001", app_id="手百lite")
+    ma.request_data(product="baiduboxapp", v_appid="1", app_id="手百")
