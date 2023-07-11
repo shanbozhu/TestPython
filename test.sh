ec2f80c01e2869d17938c357ebdec4884684997d
zhushanbo <zhushanbo@baidu.com>
2023-07-11 16:01:01 +0800

    no message

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# usage:
+# ./mqrencode.sh "字符串"
+
+time=$(date "+%Y-%m-%d_%H-%M-%S")
+prefix="/Users/wsc/Desktop/mqrencode/"
+suffix=".png"
+
+qrencode -o "${prefix}${time}${suffix}" -s 10 -m 1 "zhushanbo"
+open ${prefix}
