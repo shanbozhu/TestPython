1160271f5821e483a8829084607225f66f5d0fe2
zhushanbo <zhushanbo@baidu.com>
2023-07-20 13:58:35 +0800

    no message

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-with open('a.txt', 'a') as f:
-    f.write("\nPython教程")
+def alter(file, old_str, new_str):
+    # """
+    # 替换文件中的字符串
+    # :param file:文件名
+    # :param old_str:就字符串
+    # :param new_str:新字符串
+    # :return:
+    #
+    # """
+    file_data = ""
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            if old_str in line:
+                line = line.replace(old_str, new_str)
+            file_data += line
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(file_data)
+
+
+alter("my_file.txt", "中文网", "abc")
+# alter()
