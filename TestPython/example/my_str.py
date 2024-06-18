72c6e509236c070473b8cbc754a81d8e76746cb9
zhushanbo <zhushanbo@baidu.com>
2024-06-18 20:17:21 +0800

    no message

--- TestPython/example/my_str.py
+++ TestPython/example/my_str.py
@@ -66,19 +66,19 @@ print( len(str1.encode()) )
 str1 = "人生苦短，我用Python"
 print( len(str1.encode('gbk')) )
 
-str = "C语言中文网 >>> c.biancheng.net"
-list1 = str.split() # 采用默认 空白字符 分隔符进行分割
+str2 = "C语言中文网 >>> c.biancheng.net"
+list1 = str2.split() # 采用默认 空白字符 分隔符进行分割
 print(list1)
-list2 = str.split('>>>') # 采用多个字符进行分割
+list2 = str2.split('>>>') # 采用多个字符进行分割
 print(list2)
-list3 = str.split('.')
+list3 = str2.split('.')
 print(list3)
-list4 = str.split(' ',1) # 采用空格进行分割，并规定最多只能分割 1 次
+list4 = str2.split(' ',1) # 采用空格进行分割，并规定最多只能分割 1 次
 print(list4) # ['C语言中文网', '>>> c.biancheng.net']
-list5 = str.split('>') # 采用 > 字符进行分割
+list5 = str2.split('>') # 采用 > 字符进行分割
 print(list5) # ['C语言中文网 ', '', '', ' c.biancheng.net']
-str = "C语言中文网   >>>   c.biancheng.net"  # 包含 3 个连续的空格
-list6 = str.split()
+str2 = "C语言中文网   >>>   c.biancheng.net"  # 包含 3 个连续的空格
+list6 = str2.split()
 print(list6)
 
 # 将列表中的字符串合并成一个字符串。
@@ -93,3 +93,31 @@ di = '', 'usr', 'bin', 'env'
 print(type(di))
 a = '/'.join(di)
 print(a)
+
+# Python count()方法：统计字符串出现的次数
+
+# 前面章节介绍了如何使用 % 操作符对各种类型的数据进行格式化输出，这是早期 Python 提供的方法。自 Python 2.6 版本开始，字符串类型（str）提供了 format() 方法对字符串进行格式化。
+# Python format()格式化输出方法详解
+s = "网站名称：{:>9s}\t网址：{:s}"
+print(s.format("C语言中文网", "c.biancheng.net")) # 网站名称：   C语言中文网	网址：c.biancheng.net
+
+# 在实际开发中，数值类型有多种显示需求，比如货币形式、百分比形式等，使用 format() 方法可以将数值格式化为不同的形式。
+# 以货币形式显示
+print("货币形式：{:,d}".format(1000000))
+# 科学计数法表示
+print("科学计数法：{:E}".format(1200.12))
+# 以十六进制表示
+print("100的十六进制：{:#x}".format(100))
+# 输出百分比形式
+print("0.01的百分比表示：{:.0%}".format(0.01))
+
+s = "C语言中文网"
+print(s.encode())
+s = "C语言中文网"
+print(s.encode('GBK'))
+a = s.encode()
+print(a.decode())
+
+print(dir(str))
+print(help(str.lower))
+print(dir(dict))
