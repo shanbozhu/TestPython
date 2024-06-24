dedbd36df16041c4c4986a2a754f55b28a265b75
zhushanbo <zhushanbo@baidu.com>
2024-06-24 10:53:02 +0800

    no message

--- TestPython/example/my_special_func.py
+++ TestPython/example/my_special_func.py
@@ -321,3 +321,43 @@ funB = funA(funB)
 funB("http://c.biancheng.net/python")
 
 # *args 和 **kwargs 表示接受任意数量和类型的参数。
+
+def intNum():
+    print("开始执行")
+    for i in range(5):
+        yield i
+        print("继续执行")
+num = intNum()
+# 由此，我们就成功创建了一个 num 生成器对象。显然，和普通函数不同，intNum() 函数的返回值用的是 yield 关键字，而不是 return 关键字，此类函数又成为生成器函数。
+# 和 return 相比，yield 除了可以返回相应的值，还有一个更重要的功能，即每当程序执行完该语句时，程序就会暂停执行。不仅如此，即便调用生成器函数，Python 解释器也不会执行函数中的代码，它只会返回一个生成器（对象）。
+# 要想使生成器函数得以执行，或者想使执行完 yield 语句立即暂停的程序得以继续执行，有以下 2 种方式：
+# 通过生成器（上面程序中的 num）调用 next() 内置函数或者 __next__() 方法；
+# 通过 for 循环遍历生成器。
+
+# 调用 next() 内置函数
+print(next(num))
+# 开始执行
+# 0
+# 调用 __next__() 方法
+print(num.__next__())
+# 继续执行
+# 1
+# 通过for循环遍历生成器
+for i in num:
+    print(i)
+# 继续执行
+# 2
+# 继续执行
+# 3
+# 继续执行
+# 4
+# 继续执行
+
+# 除此之外，还可以使用 list() 函数和 tuple() 函数，直接将生成器能生成的所有值存储成列表或者元组的形式。例如：
+num = intNum()
+print(list(num))
+
+num = intNum()
+print(tuple(num))
+# 通过输出结果可以判断出，list() 和 tuple() 底层实现和 for 循环的遍历过程是类似的。
+# 相比迭代器，生成器最明显的优势就是节省内存空间，即它不会一次性生成所有的数据，而是什么时候需要，什么时候生成。
