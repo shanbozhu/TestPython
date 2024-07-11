e30233b49c068910f313f2f597053f88d87bf451
zhushanbo <zhushanbo@baidu.com>
2024-07-11 21:33:46 +0800

    no message

--- main.sh
+++ main.sh
@@ -23,12 +23,12 @@ if [ $? -eq 0 ]; then
   # requirements.txt文件是否存在
   requirements="$current_dir/requirements.txt"
   if [ -f "$requirements" ]; then
-    pip3 freeze > installed_packages.txt
+    pip3 freeze > .installed_packages.txt
     if [ $? -ne 0 ]; then
       echo "pip freeze 执行失败，请重试。"
       exit 1
     fi
-    diff "$current_dir/requirements.txt" "$current_dir/installed_packages.txt" &> /dev/null
+    diff "$current_dir/requirements.txt" "$current_dir/.installed_packages.txt" &> /dev/null
     if [ $? -ne 0 ]; then
       echo "虚拟环境中模块缺失，正在安装..."
       pip3 install -r requirements.txt &> /dev/null
@@ -41,8 +41,10 @@ if [ $? -eq 0 ]; then
     echo "没有模块列表文件，无法在虚拟环境中批量自动安装模块。Python脚本可能会执行失败！！！"
     echo "若Python脚本执行失败，提示缺少模块，请按照下面步骤手动执行。"
     echo "  1. 执行命令激活虚拟环境：source $venv_dir/bin/activate"
-    echo "  2. 在虚拟环境中安装缺失的模块：pip3 install xyz，xyz表示缺失的模块"
-    echo "  3. 执行执行Python脚本：$current_dir/$python_file"
+    echo "  2. 在虚拟环境中安装缺失的模块：pip3 install xyz，xyz表示缺失的模块。"
+    # echo "  3. 执行Python脚本：$current_dir/$python_file"
+    echo "  3. 执行Python脚本：./main.sh"
+    echo "  4. 重复步骤2和3安装所有缺失的模块，直至Python脚本执行成功。"
   fi
 else
   echo "虚拟环境激活失败，请重试。"
